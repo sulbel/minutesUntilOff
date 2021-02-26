cf7252aabb6372d70934759e1c779ea97353c95f
sulbel <sulbel20@gmail.com>
2021-02-25 22:37:02 -0600

    v0.01 - added print statements

--- workEnd.py
+++ workEnd.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 
+# TO-DO: error-checking
 def hour_min_2_mins(hour, minute):
     return((60*hour) + minute)
 
+# TO-DO: way to toggle earlyWeek bool
 earlyWeek = True
 if(earlyWeek):
     endHour = 15
@@ -12,22 +14,23 @@ else:
     endHour = 16
     endMinute = 30
 
-
-#currTime = datetime.now()
+# Print statements commented out are for debugging
+currTime = datetime.now()
 #print(currTime.hour, currTime.minute)
-currTimehour = 10
-currTimeminute = 42
+#currTimehour = 10
+#currTimeminute = 42
 
 endMinutes = hour_min_2_mins(endHour, endMinute)
 #print(hour_min_2_mins(endHour, endMinute))
-currentMinutes = hour_min_2_mins(currTimehour, currTimeminute)
-#print(hour_min_2_mins(currTimehour, currTimeminute))
+currentMinutes = hour_min_2_mins(currTime.hour, currTime.minute)
+#print(hour_min_2_mins(currTime.hour, currTime.minute))
 
 totalMinutesLeft = endMinutes - currentMinutes
 #print(totalMinutesLeft)
-
 hoursLeft = int(totalMinutesLeft/60)
 #print(hoursLeft)
 minutesLeft = totalMinutesLeft % 60
 #print(minutesLeft)
 
+print("There are:", totalMinutesLeft, "total minutes of work left.")
+print("You can clock out in", hoursLeft, "hours and", minutesLeft, "minutes.\n")
